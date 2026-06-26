d8041ec722f0f5cc073ba9849a27237d6ccd8450
Daria Levchuk <darina.levchuk@gmail.com>
2026-06-26 13:08:31 +0300

    changes after mentor's comment

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,13 @@
 #output "ecr_repository_url" {
 #  value = module.ecr.repository_url
 #}
+
+output "cluster_id" {
+  description = "The ID/name of the EKS cluster"
+  value       = module.eks.cluster_id
+}
+
+output "cluster_endpoint" {
+  description = "The endpoint for the EKS Kubernetes API"
+  value       = module.eks.cluster_endpoint
+}
